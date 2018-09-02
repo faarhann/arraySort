6767aaff1a4fd16baebd8afc29f4c30a671dfdde
faarhann <farhanmohamed@hotmail.se>
2018-09-02 10:06:36 +0200

    sorted array

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scan = new Scanner(System.in);
+    public static void main(String[] args) {
+
+
+        int[] myIntegers = getIntegers(5);
+        int[] sorted = sortIntegers(myIntegers);
+        printArray(sorted);
+
+
+
+
+    }
+
+    public static int[] getIntegers(int capacity){
+
+        int[] newArray = new int[capacity];
+
+        System.out.println("Enter values for the array!");
+        for (int i=0; i< newArray.length; i++){
+            newArray[i] = scan.nextInt();
+        }
+
+        return newArray;
+    }
+
+    public static void printArray(int[] array){
+        for (int i=0; i<array.length; i++){
+            System.out.println("Element: " + i + " Value " + array[i]);
+        }
+    }
+
+    public static int[] sortIntegers(int[] array){
+        int[] sortedArray = new int[array.length];
+        for (int i=0; i<array.length; i++){
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while (flag){
+            flag = false;
+            for (int i=0; i<sortedArray.length-1; i++){
+                if (sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i+1] = temp;
+                    flag = true;
+                }
+            }
+        }
+        return sortedArray;
+    }
+}
